5a843a38693b2b2dc3a87361421da0eed9fa98e7
Arthur Henrique Simione Pouza <arthur.pouza@etec.sp.gov.br>
2025-09-06 09:29:20 -0300

    Delete duduzin_thurpoza directory

--- duduzin_thurpoza/attduduzin/bdn.sql
+++
@@ -1,9 +0,0 @@
-create database bdn;
-use bdn;
-
-create table personas (
-    id int auto_increment primary key,
-    nome varchar(255) not null,
-    cpf varchar(15) unique not null,
-    telefone varchar(20) not null
-);
